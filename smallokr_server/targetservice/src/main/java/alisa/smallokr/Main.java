5b34f57e78c1cdcb1b9d33fc8b86051835df96b6
sunyubo <sunyubo.alisa@outlook.com>
2024-04-03 23:47:44 +0800

    服务注册以及远程调用简单实现

--- smallokr_server/targetservice/src/main/java/alisa/smallokr/Main.java
+++ smallokr_server/targetservice/src/main/java/alisa/smallokr/Main.java
@@ -3,6 +3,10 @@ package alisa.smallokr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,4 +14,11 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+    @RestController
+	class EchoController {
+		@RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+		public String echo(@PathVariable String string) {
+			return "Hello Nacos Discovery " + string;
+		}
+	}
 }
