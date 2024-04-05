4c4fd8b2be778af5d3dcc738102a5aaecc7d75bf
sunyubo <sunyubo.alisa@outlook.com>
2024-04-05 16:58:10 +0800

    测试seata成功

--- smallokr_server/targetservice/src/main/java/alisa/smallokr/service/TestService.java
+++ smallokr_server/targetservice/src/main/java/alisa/smallokr/service/TestService.java
@@ -2,25 +2,20 @@ package alisa.smallokr.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import alisa.smallokr.POJO.Test;
-import alisa.smallokr.mapper.TestMapper;
-import io.seata.common.exception.NotSupportYetException;
 import io.seata.spring.annotation.GlobalTransactional;
 
 @Service
 public class TestService {
-    @Autowired
-    private TestMapper testMapper;
     @Autowired
     private TestClient testClient;
 
     @GlobalTransactional
     public void add(Test test) throws Exception {
         try {
-            testMapper.add(test);
+            // 测试分支事物
             testClient.add(test);
-            throw new NotSupportYetException("test");
+            throw new RuntimeException("test");
         } catch (Exception e) {
             throw e;
         }
