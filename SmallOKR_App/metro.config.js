2f5e72315cece4dede3d44d889b0f5cf3c51b404
SunYubo <52072505+sunyuboalisa@users.noreply.github.com>
2024-09-05 08:32:13 +0800

    修改登录界面ui

--- SmallOKR_App/metro.config.js
+++ SmallOKR_App/metro.config.js
@@ -10,6 +10,9 @@ const config = {
   server: {
     port: 8081, // 指定你想要的端口号
   },
+  resolver: {
+    assetExts: ['png', 'jpg', 'jpeg', 'gif', 'svg', 'ttf', 'mp4', 'wav', 'ogg'], // 处理这些文件类型
+  },
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
--- SmallOKR_App/src/screens/LoginScreen.tsx
+++ SmallOKR_App/src/screens/LoginScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, { useContext, useState } from 'react';
 import {
   View,
   Text,
@@ -6,36 +6,40 @@ import {
   StyleSheet,
   TouchableOpacity,
   Alert,
+  ImageBackground,
+  Image,
 } from 'react-native';
-import {UserDispatchContext} from '../state/UserContext';
-import {UserService} from '../service/BusiService';
-import {axiosHelper} from '../util/AxiosHelper';
+import { UserDispatchContext } from '../state/UserContext';
+import { UserService } from '../service/BusiService';
+import { axiosHelper } from '../util/AxiosHelper';
 
-export const LoginScreen = ({}) => {
+export const LoginScreen = ({ }) => {
   const dispatch = useContext(UserDispatchContext);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
-  const handleLogin = () => {
-    UserService.login({username: username, password: password})
-      .then(res => {
-        if (res.data.code === '200') {
-          dispatch({
-            type: 'Login',
-            user: {username: username, password: password},
-          });
-          console.log('登录:', res.data.data);
-          axiosHelper.setToken(res.data.data);
-        } else {
-          setError('用户名或密码错误');
-        }
-      })
-      .catch(err => Alert.alert(err));
+  const handleLogin = async () => {
+    try {
+      const res=await UserService.login({ username: username, password: password });
+      if (res.data.code === '200') {
+        dispatch({
+          type: 'Login',
+          user: { username: username, password: password },
+        });
+        axiosHelper.setToken(res.data.data);
+      } else {
+        setError('用户名或密码错误');
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <View style={styles.container}>
+    <ImageBackground
+      source={require('../../assets/imgs/login-background.png')}
+      style={styles.container}>
       <TextInput
         autoCapitalize="none"
         style={styles.input}
@@ -55,7 +59,7 @@ export const LoginScreen = ({}) => {
       <TouchableOpacity onPress={handleLogin} style={styles.login}>
         <Text children="登录" style={styles.loginText} />
       </TouchableOpacity>
-    </View>
+    </ImageBackground>
   );
 };
 
@@ -71,25 +75,31 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   input: {
-    height: 40,
+    height: 50,
     width: '100%',
-    borderColor: 'gray',
-    borderWidth: 1,
     marginBottom: 10,
-    paddingHorizontal: 10,
+    paddingHorizontal: 5,
+    borderBottomWidth: 1,
+    borderColor: 'gray',
+    justifyContent: 'center',
   },
   error: {
     color: 'red',
     marginBottom: 10,
   },
   login: {
-    width: '100%',
-    backgroundColor: 'blue',
+    width: '80%',
+    height:50,
+    marginTop:30,
+    backgroundColor: '#007eff',
     marginHorizontal: 5,
+    borderRadius:25,
+    justifyContent:'center'
   },
   loginText: {
     fontSize: 24,
     textAlign: 'center',
+    color:'white'
   },
 });
 
