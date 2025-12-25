7e4de122d2889a51f78abfe4e2ae96d694f2d379
sunyubo <sunyubo.alisa@outlook.com>
2025-12-25 12:36:51 +0800

    修改返回值类型

--- smallokr_server/common/src/main/java/com/alisa/Util/Result.java
+++ smallokr_server/common/src/main/java/com/alisa/Util/Result.java
@@ -4,12 +4,12 @@ import lombok.Data;
 
 @Data
 public class Result<T> {
-    private String code;
+    private int code;
     private String message;
     private T data;
 
     public Result(T data) {
-        this.code = "200";
+        this.code = 200;
         this.message = "成功";
         this.data = data;
     }
