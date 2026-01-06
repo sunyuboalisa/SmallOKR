84a2a2b0be9a3762716ae68c19bac12f5195b2ac
syb <sunyubo.alisa@outlook.com>
2026-01-06 17:22:45 +0800

    适配新的cloud

--- smallokr_server/common/src/main/java/com/alisa/Util/UUIDTool.java
+++
@@ -1,9 +0,0 @@
-package com.alisa.util;
-
-import java.util.UUID;
-
-public class UUIDTool {
-    public static String getUUID(){
-        return UUID.randomUUID().toString().replace("-","");
-    }
-}
